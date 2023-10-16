49ed4602a3a2614c8630a121dcbbc3865973875f
Mohamed <melsayed@example.com>
2023-10-16 06:46:35 -0700

    neccessary merge

--- _printf.c
+++ _printf.c
@@ -13,12 +13,12 @@ int _printf(const char *format, ...)
 	char *p, *start;
 	params_t params = PARAMS_INIT;
 
-	va_start(ap,format);
+	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1])
-		return(-1);
+		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return(-1);
+		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap)
@@ -38,7 +38,8 @@ int _printf(const char *format, ...)
 		if get_modifier(p, &params);
 			p++;
 		if (!get_specifier(p))
-			sum += print_from_to(start, p, params.1_modifier || params.h_modifier ? p - 1 : 0);
+			sum += print_from_to(start, p, params.1_modifier ||
+				params.h_modifier ? p - 1 : 0);
 		else
 			sum += get_print_func(p, ap, &params);
 	}
