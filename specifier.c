8dba5bf348b45e3c2f9ac4c572648b561639e05d
Mohamed <melsayed@example.com>
2023-10-16 13:20:17 -0700

    printf

--- specifier.c
+++ specifier.c
@@ -35,3 +35,103 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 	}
 	return (NULL);
 }
+
+/**
+ * get_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: number of bytes printed
+ */
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - finds the flag func
+ * @s: the format string
+ * @params: the parameter struct
+ *
+ * Return: (1) if flag was valid
+ */
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+'
+			i = params->plus_flag = 1;
+			break;
+		case ' '
+			i = params->space_flag = 1;
+			break;
+		case '#'
+			i = params->hashtag_flag = 1;
+			break;
+		case '-'
+			i = params->minus_flag = 1;
+			break;
+		case '0'
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+  * get_modifier - finds the modifier function
+  *
+  * @s: the format string
+  * @params: the parameter struct
+  *
+  * Return: (1) if modifier was valid
+  */
+
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*S)
+	{
+		case 'h';
+			i = params->h_modifier = 1;
+			break;
+		case 'l';
+			i = params->l_modifier = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_width - gets with from the format string
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: new pointer
+ */
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
