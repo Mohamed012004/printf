dcd922496964cd290828f3a6c917347ff4f83fc2
Mohamed <melsayed@example.com>
2023-10-17 10:30:46 -0700

    printf

--- specifier.c
+++ specifier.c
@@ -19,7 +19,7 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 		{"u", print_unsigned},
 		{"x", print_hex},
 		{"X", print_HEX},
-		{"p", print_address},
+		{"p", print_adress},
 		{"S", print_S},
 		{"r", print_rev},
 		{"R", print_rot13},
@@ -30,7 +30,7 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 	while (specifiers[i].specifier)
 	{
 		if (*s == specifiers[i].specifier[0])
-			return (specifiers[i].f)
+			return (specifiers[i].f);
 		i++;
 	}
 	return (NULL);
@@ -66,19 +66,19 @@ int get_flag(char *s, params_t *params)
 
 	switch (*s)
 	{
-		case '+'
+		case '+':
 			i = params->plus_flag = 1;
 			break;
-		case ' '
+		case ' ':
 			i = params->space_flag = 1;
 			break;
-		case '#'
+		case '#':
 			i = params->hashtag_flag = 1;
 			break;
-		case '-'
+		case '-':
 			i = params->minus_flag = 1;
 			break;
-		case '0'
+		case '0':
 			i = params->zero_flag = 1;
 			break;
 	}
@@ -98,12 +98,12 @@ int get_modifier(char *s, params_t *params)
 {
 	int i = 0;
 
-	switch (*S)
+	switch (*s)
 	{
-		case 'h';
+		case 'h':
 			i = params->h_modifier = 1;
 			break;
-		case 'l';
+		case 'l':
 			i = params->l_modifier = 1;
 			break;
 	}
