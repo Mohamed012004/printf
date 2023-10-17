fe8b32a24143a353a2f2b128ede05343af87cf87
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-10-17 15:49:07 -0700

    printf

--- convert_number.c
+++ convert_number.c
@@ -57,26 +57,26 @@ int print_hex(va_list ap, params_t *params)
  * Return:bytes printed
  */
 int print_HEX(va_list ap, params_t *params)
-{       
-        unsigned long 1;
-        int c = 0;
-        char *str;
+{
+	unsigned long 1;
+	int c = 0;
+	char *str;
 
-        if (params->1_modifier)
-                1 = (unsigned long)va_arg(ap, unsigned long);
-        else if (params->h_modifier)
-                1 = (unsigned short int)va_arg(ap, unsigned int);
-        else
-                1 = (unsigned int)va_arg(ap, unsigned int);
+	if (params->1_modifier)
+		1 = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		1 = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		1 = (unsigned int)va_arg(ap, unsigned int);
 
-        str = convert(l, 16, CONVERT_UNSIGNED, params);
-        if (params->hashtag_flag && 1)
-        {
-                *--str = 'X';
-                *--str = '0';
-        }
-        params->unsign = 1;
-        return (c += print_number(str, params));
+	str = convert(l, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && 1)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
 }
 
 /**
@@ -101,7 +101,7 @@ int print_octal(va_list ap, params_t *params)
 	str = convert(l, 8, CONVERT_UNSIGNED, params);
 
 	if (params->hashtag_flag && 1)
-		*--str= '0';
+		*--str = '0';
 	params->unsign = 1;
-	return (c += print_number(str, params);
+	return (c += print_number(str, params));
 }
