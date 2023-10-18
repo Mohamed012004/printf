1af25d6b10fa159aa62e24709f9ea436dc1da971
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2023-10-17 17:55:35 -0700

    printf Done

--- convert_number.c
+++ convert_number.c
@@ -100,7 +100,7 @@ int print_octal(va_list ap, params_t *params)
 		l = (unsigned int)va_arg(ap, unsigned int);
 	str = convert(l, 8, CONVERT_UNSIGNED, params);
 
-	if (params->hashtag_flag && 1)
+	if (params->hashtag_flag && l)
 		*--str = '0';
 	params->unsign = 1;
 	return (c += print_number(str, params));
